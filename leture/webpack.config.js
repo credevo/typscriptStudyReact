8302e4446127543f4ab73c765b5ad59e8741c513
credevo <62835973+credevo@users.noreply.github.com>
2022-09-14 08:55:24 +0900

    init wepback project setting

--- leture/webpack.config.js
+++ leture/webpack.config.js
@@ -3,11 +3,22 @@ const webpack = require('webpack');
 
 module.exports = {
     mode : 'development',
-    devtool : 'evel',
+    devtool : 'source-map',
     resolve : {
         extensions : ['.jsx','.js','.tsx','.ts']
     },
     entry : {
         app : './client'
+    },
+    module : {
+        rules : [{
+            test:/\.tsx?$/,
+            loader : 'ts-loader' 
+        }]
+    },
+    plugins : [],
+    output : {
+        filename : '[name].js',
+        path : path.join(__dirname,'dist')
     }
 }
